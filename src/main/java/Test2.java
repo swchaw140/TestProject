e50488d6da99de360468a5a39706c7672cde3d00
swati140 <chawlaswati140@gmail.com>
2022-06-29 04:30:35 +0530

    Added TestNgxml and file to Maven project

---
+++ src/main/java/Test2.java
@@ -0,0 +1,25 @@
+import org.testng.annotations.Test;
+
+public class Test2 {
+
+    @Test
+    public void Method5()
+    {
+        System.out.println("Method 1");
+    }
+    @Test
+    public void Method6()
+    {
+        System.out.println("Method 2");
+    }
+    @Test
+    public void Method7()
+    {
+        System.out.println("Method 3");
+    }
+    @Test
+    public void Method8()
+    {
+        System.out.println("Method 4");
+    }
+}
---
+++ src/test/java/Test1.java
@@ -0,0 +1,27 @@
+import org.testng.annotations.Test;
+
+public class Test1 {
+    public static void main(String args[]){
+        System.out.println("Hello World");
+    }
+    @Test
+    public void Method1()
+    {
+        System.out.println("Method 1");
+    }
+    @Test
+    public void Method2()
+    {
+        System.out.println("Method 2");
+    }
+    @Test
+    public void Method3()
+    {
+        System.out.println("Method 3");
+    }
+    @Test
+    public void Method4()
+    {
+        System.out.println("Method 4");
+    }
+}
